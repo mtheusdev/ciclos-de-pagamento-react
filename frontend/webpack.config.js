87b9debd19d3559fa4f3cf5e9869163fb00c4c0f
mtheusdev <matheus.santos@edu.udesc.br>
2021-05-19 20:51:03 -0300

    criando index.jsx e app.jsx

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,10 +2,10 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   output: {
     path: __dirname + "/public",
-    filename: "./app.js",
+    filename: "./app.jsx",
   },
   devServer: {
     port: 8080,
