1d01b662d8083083d34caf1752353a742103a06a
mtheusdev <matheus.santos@edu.udesc.br>
2021-05-26 20:59:02 -0300

    adicionando logica e componentes para as tabs dos ciclos

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import DashBoardReducer from "../dashboard/dashboardReducer";
+import TabReducer from "../common/tab/tabReducer";
 const rootReducer = combineReducers({
   dashboard: DashBoardReducer,
+  tab: TabReducer,
 });
 
 export default rootReducer;
